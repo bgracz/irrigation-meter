87308409fd37257144a50afa0cc1965afd563194
B <gracz.blazej@gmail.com>
2023-06-06 12:40:34 +0200

    Chart with load data from localStorage added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Buttons from './Buttons';
 import Bottle from './img/water-bottle.png';
+import LineChart from './Chart';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <img src={Bottle} className="bottle-image" alt="bottle-top"></img>
       </div>
       <Buttons />
+      <LineChart />
     </div>
   );
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,12 +1,30 @@
 import React from "react";
-import 'Chart.css';
+import './Chart.css';
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
-function Chart() {
+Chart.defaults.color = '#fff';
+
+const labels = Object.keys(localStorage).slice(Object.keys(localStorage).length - 7);
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Daily water consumption",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: Object.values(localStorage).slice(Object.values(localStorage).length - 7)
+    },
+  ],
+};
+
+function LineChart() {
     return (
         <div className="chart">
-
+            <Line className="chart" data={data} />
         </div>
     )
 }
 
-export default Chart;
+export default LineChart;
